312ed896a1bd1d12282b0f44a9b16a7f42c4cb90
Abhishek Upadhyay <138516877+Abhi-upadhyay2109@users.noreply.github.com>
2025-03-02 20:29:44 +0530

    Update server.js
    add

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // ✅ Use CORS only once with proper settings
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow frontend URL
+    origin: "https://yoga12.netlify.app/", // Allow frontend URL
     credentials: true, // Allow cookies & authentication headers
   })
 );
